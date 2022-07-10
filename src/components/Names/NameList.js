290f5996d2c9102dede85178e8d1938d90d5b19c
Oleg Ghimp <ghimpco@gmail.com>
2022-07-10 23:24:10 +0300

    following course

---
+++ src/components/Names/NameList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NameList = props => {
+
+    function formatName(props) {
+        props.map(person => person.firstName)
+
+    }
+
+    return <p>{formatName()}</p>;
+}
+
+
+export default NameList;
